613488260cc323543aa37bcab5cd0d7835975d70
wf-kuadrat <ferianggri54@gmail.com>
2024-08-26 21:21:41 +0700

    pertama fix

---
+++ javacript/produk.js
@@ -0,0 +1,175 @@
+function sendToWhatsapp() {
+      let number = "+6285713133074"
+
+      let name = document.getElementById("user-name").value;
+      let alamat = document.getElementById("alamat-name").value;
+      let produk = document.getElementById("produk-name").value;
+      let harga = document.getElementById("harga-name").value;
+      let keterangan = document.getElementById("keterangan-name").value;
+
+      var url = "https://wa.me/" + number + "?text=" +
+            "Nama Saya : " + name + "%0a" +
+            "Alamat Saya : " + alamat + "%0a" +
+            "Saya mau beli produk :" + produk + "%0a" +
+            "Dengan Harga : " + harga + "%0a" +
+            "Keterangan : " + keterangan + "%0a%0a "
+
+      window.open(url, '_blank').focus();
+}
+
+function sendToWhatsappKedua() {
+      let numberDua = "+6285713133074"
+
+      let nameDua = document.getElementById("user-name-dua").value;
+      let alamatDua = document.getElementById("alamat-name-dua").value;
+      let produkDua = document.getElementById("produk-name-dua").value;
+      let hargaDua = document.getElementById("harga-name-dua").value;
+      let keteranganDua = document.getElementById("keterangan-name-dua").value;
+
+      var urlDua = "https://wa.me/" + numberDua + "?text=" +
+            "Nama Saya : " + nameDua + "%0a" +
+            "Alamat Saya : " + alamatDua + "%0a" +
+            "Saya mau beli produk :" + produkDua + "%0a" +
+            "Dengan Harga : " + hargaDua + "%0a" +
+            "Keterangan : " + keteranganDua + "%0a%0a "
+
+      window.open(urlDua, '_blank').focus();
+
+}
+
+function sendToWhatsappKetiga() {
+      let numberTiga = "+6285713133074"
+
+      let nameTiga = document.getElementById("user-name-tiga").value;
+      let alamatTiga = document.getElementById("alamat-name-tiga").value;
+      let produkTiga = document.getElementById("produk-name-tiga").value;
+      let hargaTiga = document.getElementById("harga-name-tiga").value;
+      let keteranganTiga = document.getElementById("keterangan-name-tiga").value;
+
+      var urlTiga = "https://wa.me/" + numberTiga + "?text=" +
+            "Nama Saya : " + nameTiga + "%0a" +
+            "Alamat Saya : " + alamatTiga + "%0a" +
+            "Saya mau beli produk :" + produkTiga + "%0a" +
+            "Dengan Harga : " + hargaTiga + "%0a" +
+            "Keterangan : " + keteranganTiga + "%0a%0a "
+
+      window.open(urlTiga, '_blank').focus();
+
+}
+
+function sendToWhatsappKeempat() {
+      let numberEmpat = "+6285713133074"
+
+      let nameEmpat = document.getElementById("user-name-empat").value;
+      let alamatEmpat = document.getElementById("alamat-name-empat").value;
+      let produkEmpat = document.getElementById("produk-name-empat").value;
+      let hargaEmpat = document.getElementById("harga-name-empat").value;
+      let keteranganEmpat = document.getElementById("keterangan-name-empat").value;
+
+      var urlEmpat = "https://wa.me/" + numberEmpat + "?text=" +
+            "Nama Saya : " + nameEmpat + "%0a" +
+            "Alamat Saya : " + alamatEmpat + "%0a" +
+            "Saya mau beli produk :" + produkEmpat + "%0a" +
+            "Dengan Harga : " + hargaEmpat + "%0a" +
+            "Keterangan : " + keteranganEmpat + "%0a%0a "
+
+      window.open(urlEmpat, '_blank').focus();
+
+}
+
+function sendToWhatsappKelima() {
+      let numberLima = "+6285713133074"
+
+      let nameLima = document.getElementById("user-name-lima").value;
+      let alamatLima = document.getElementById("alamat-name-lima").value;
+      let produkLima = document.getElementById("produk-name-lima").value;
+      let hargaLima = document.getElementById("harga-name-lima").value;
+      let keteranganLima = document.getElementById("keterangan-name-lima").value;
+
+      var urlLima = "https://wa.me/" + numberLima + "?text=" +
+            "Nama Saya : " + nameLima + "%0a" +
+            "Alamat Saya : " + alamatLima + "%0a" +
+            "Saya mau beli produk :" + produkLima + "%0a" +
+            "Dengan Harga : " + hargaLima + "%0a" +
+            "Keterangan : " + keteranganLima + "%0a%0a "
+
+      window.open(urlLima, '_blank').focus();
+
+}
+
+function sendToWhatsappKeenam() {
+      let numberEnam = "+6285713133074"
+
+      let nameEnam = document.getElementById("user-name-enam").value;
+      let alamatEnam = document.getElementById("alamat-name-enam").value;
+      let produkEnam = document.getElementById("produk-name-enam").value;
+      let hargaEnam = document.getElementById("harga-name-enam").value;
+      let keteranganEnam = document.getElementById("keterangan-name-enam").value;
+
+      var urlEnam = "https://wa.me/" + numberEnam + "?text=" +
+            "Nama Saya : " + nameEnam + "%0a" +
+            "Alamat Saya : " + alamatEnam + "%0a" +
+            "Saya mau beli produk :" + produkEnam + "%0a" +
+            "Dengan Harga : " + hargaEnam + "%0a" +
+            "Keterangan : " + keteranganEnam + "%0a%0a "
+
+      window.open(urlEnam, '_blank').focus();
+
+}
+
+function sendToWhatsappKetujuh() {
+      let numberTujuh = "+6285713133074"
+
+      let nameTujuh = document.getElementById("user-name-tujuh").value;
+      let alamatTujuh = document.getElementById("alamat-name-tujuh").value;
+      let produkTujuh = document.getElementById("produk-name-tujuh").value;
+      let hargaTujuh = document.getElementById("harga-name-tujuh").value;
+      let keteranganTujuh = document.getElementById("keterangan-name-tujuh").value;
+
+      var urlTujuh = "https://wa.me/" + numberTujuh + "?text=" +
+            "Nama Saya : " + nameTujuh + "%0a" +
+            "Alamat Saya : " + alamatTujuh + "%0a" +
+            "Saya mau beli produk :" + produkTujuh + "%0a" +
+            "Dengan Harga : " + hargaTujuh + "%0a" +
+            "Keterangan : " + keteranganTujuh + "%0a%0a "
+
+      window.open(urlTujuh, '_blank').focus();
+}
+
+function sendToWhatsappKedelapan() {
+      let numberDelapan = "+6285713133074"
+
+      let nameDelapan = document.getElementById("user-name-delapan").value;
+      let alamatDelapan = document.getElementById("alamat-name-delapan").value;
+      let produkDelapan = document.getElementById("produk-name-delapan").value;
+      let hargaDelapan = document.getElementById("harga-name-delapan").value;
+      let keteranganDelapan = document.getElementById("keterangan-name-delapan").value;
+
+      var urlDelapan = "https://wa.me/" + numberDelapan + "?text=" +
+            "Nama Saya : " + nameDelapan + "%0a" +
+            "Alamat Saya : " + alamatDelapan + "%0a" +
+            "Saya mau beli produk :" + produkDelapan + "%0a" +
+            "Dengan Harga : " + hargaDelapan + "%0a" +
+            "Keterangan : " + keteranganDelapan + "%0a%0a "
+
+      window.open(urlDelapan, '_blank').focus();
+}
+
+function sendToWhatsappKesembilan() {
+      let numberSembilan = "+6285713133074"
+
+      let nameSembilan = document.getElementById("user-name-sembilan").value;
+      let alamatSembilan = document.getElementById("alamat-name-sembilan").value;
+      let produkSembilan = document.getElementById("produk-name-sembilan").value;
+      let hargaSembilan = document.getElementById("harga-name-sembilan").value;
+      let keteranganSembilan = document.getElementById("keterangan-name-sembilan").value;
+
+      var urlSembilan = "https://wa.me/" + numberSembilan + "?text=" +
+            "Nama Saya : " + nameSembilan + "%0a" +
+            "Alamat Saya : " + alamatSembilan + "%0a" +
+            "Saya mau beli produk :" + produkSembilan + "%0a" +
+            "Dengan Harga : " + hargaSembilan + "%0a" +
+            "Keterangan : " + keteranganSembilan + "%0a%0a "
+
+      window.open(urlSembilan, '_blank').focus();
+}
